653896fa99ddd33fd17f9758550d7d9e0d4a0fb5
Abi Gopal <agopal43@gmail.com>
2014-07-02 17:27:58 -0400

    Added public key and removed twitter

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
+exports.index = function(req, res) {
   res.render('index', { title: 'Abi Gopal' });
+};
+
+exports.key = function(req, res) {
+  res.render('key', { title: 'Abi Gopal' });
 };
